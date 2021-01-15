2d19a0208a5502539ba13261e17a465ec23803cb
Mac-lp3 <staticlp3@gmail.com>
2021-01-15 15:22:50 +1300

    removed root api config

--- tfs/api/main.tf
+++ tfs/api/main.tf
@@ -8,42 +8,42 @@ resource "aws_api_gateway_rest_api" "slapi_gateway" {
   }
 }
 
-resource "aws_api_gateway_resource" "slapi_gateway_all_proxy" {
-   rest_api_id = aws_api_gateway_rest_api.slapi_gateway.id
-   parent_id   = aws_api_gateway_rest_api.slapi_gateway.root_resource_id
-   path_part   = "{proxy+}"
+resource "aws_api_gateway_resource" "slapi_gateway_drugs_proxy" {
+  rest_api_id = aws_api_gateway_rest_api.slapi_gateway.id
+  parent_id   = aws_api_gateway_rest_api.slapi_gateway.root_resource_id
+  path_part   = "drugs/{proxy+}"
 }
 
-resource "aws_api_gateway_method" "slapi_gateway_all_proxy" {
-   rest_api_id   = aws_api_gateway_rest_api.slapi_gateway.id
-   resource_id   = aws_api_gateway_resource.slapi_gateway_all_proxy.id
-   http_method   = "ANY"
-   authorization = "NONE"
-}
-
-resource "aws_api_gateway_method" "slapi_gateway_root" {
-   rest_api_id   = aws_api_gateway_rest_api.slapi_gateway.id
-   resource_id   = aws_api_gateway_rest_api.slapi_gateway.root_resource_id
-   http_method   = "ANY"
-   authorization = "NONE"
+resource "aws_api_gateway_method" "slapi_gateway_drugs_proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.slapi_gateway.id
+  resource_id   = aws_api_gateway_resource.slapi_gateway_drugs_proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "get_drugs_lambda" {
-   rest_api_id = aws_api_gateway_rest_api.slapi_gateway.id
-   resource_id = aws_api_gateway_method.slapi_gateway_all_proxy.resource_id
-   http_method = aws_api_gateway_method.slapi_gateway_all_proxy.http_method
+  rest_api_id = aws_api_gateway_rest_api.slapi_gateway.id
+  resource_id = aws_api_gateway_method.slapi_gateway_drugs_proxy.resource_id
+  http_method = aws_api_gateway_method.slapi_gateway_drugs_proxy.http_method
 
-   integration_http_method = "GET"
-   type                    = "AWS_PROXY"
-   uri                     = var.get_drugs_lambda_arn
+  integration_http_method = "GET"
+  type                    = "AWS_PROXY"
+  uri                     = var.get_drugs_lambda_arn
 }
 
-resource "aws_api_gateway_integration" "lambda_root" {
-   rest_api_id = aws_api_gateway_rest_api.slapi_gateway.id
-   resource_id = aws_api_gateway_method.slapi_gateway_root.resource_id
-   http_method = aws_api_gateway_method.slapi_gateway_root.http_method
+# resource "aws_api_gateway_method" "slapi_gateway_root" {
+#   rest_api_id   = aws_api_gateway_rest_api.slapi_gateway.id
+#   resource_id   = aws_api_gateway_rest_api.slapi_gateway.root_resource_id
+#   http_method   = "ANY"
+#   authorization = "NONE"
+# }
 
-   integration_http_method = "GET"
-   type                    = "AWS_PROXY"
-   uri                     = var.get_drugs_lambda_arn
-}
+# resource "aws_api_gateway_integration" "root_lambda" {
+#    rest_api_id = aws_api_gateway_rest_api.slapi_gateway.id
+#    resource_id = aws_api_gateway_method.slapi_gateway_root.resource_id
+#    http_method = aws_api_gateway_method.slapi_gateway_root.http_method
+
+#    integration_http_method = "GET"
+#    type                    = "AWS_PROXY"
+#    uri                     = var.get_drugs_lambda_arn
+# }

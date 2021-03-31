c50273bb8839fdb100887ae37d4492ee615ae816
Mac-lp3 <staticlp3@gmail.com>
2021-04-01 11:56:42 +1300

    updated interfaces and added some helper functions

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -3,6 +3,30 @@
  * All main.ts functions should return an implementation of these.
  */
 
-export interface Error {};
-export interface Warning {};
-export interface Resource {};
+const ERROR_TYPE_VAL = 'ErrorPayload';
+
+export interface BasePayload {};
+export interface ErrorPayload extends BasePayload {
+    type: 'ErrorPayload';
+};
+export interface WarningPayload extends BasePayload {
+    type: 'WarningPayload';
+};
+export interface ResourcePayload extends BasePayload {
+    type: 'ResourcePayload';
+};
+
+/**
+ * type util functions.
+ */
+export function isAnError(obj: BasePayload): obj is ErrorPayload {
+    return (obj as ErrorPayload).type === 'ErrorPayload';
+};
+
+export function isAWarning(obj: BasePayload): obj is WarningPayload {
+    return (obj as WarningPayload).type === 'WarningPayload';
+};
+
+export function isAResource(obj: BasePayload): obj is ResourcePayload {
+    return (obj as ResourcePayload).type === 'ResourcePayload';
+};
